0be28bf87aeb67acc1e8f7f1a3e9c8f9357e074b
nm19010 <105315301+nm19010@users.noreply.github.com>
2023-11-04 13:04:21 -0600

    Se agrego mensaje de error
    Se agrego un Try Catch para capturar el error y asi dar mensaje expecifico en la pantalla

--- controller/horasExtras/crear.php
+++ controller/horasExtras/crear.php
@@ -1,5 +1,7 @@
 <?php
 
+$k = "SQLSTATE[23000]: [Microsoft][ODBC Driver 17 for SQL Server][SQL Server]Instrucción UPDATE en conflicto con la restricción CHECK 'CHK_NOCTURNA'. El conflicto ha aparecido en la base de datos 'rrhh', tabla 'dbo.HORAS_EXTRA'.";
+
 if(session_id() == '' || !isset($_SESSION) || session_status() === PHP_SESSION_NONE) {
     session_start();
 }
@@ -7,24 +9,33 @@ if(session_id() == '' || !isset($_SESSION) || session_status() === PHP_SESSION_N
 include("../../model/conexion.php");
 include("funciones.php");
 
-if ($_POST["operacion"] == "Crear" or $_POST["operacion"] == "Editar") {
-    $stmt = $conexion->prepare("EXECUTE PROC_INS_UPD_HORAEXTRA :id_horEx, :fecha_horaExtra, :tipo, :cantidad, :id_empleado");
-
-    $resultado = $stmt->execute(
-        array(
-            ':id_horEx'    => $_POST["id_horEx"],
-            ':fecha_horaExtra'    => $_POST["fecha_horaExtra"],
-            ':tipo'    => $_POST["tipo"],
-            ':cantidad'    => $_POST["cantidad"],
-            ':id_empleado'    => $_POST["id_empleado"]
-        )
-    );
-
-    if (!empty($resultado)) {
-        if ($_POST["operacion"] == "Crear") {
-            echo 'Registro creado';
-        } else {
-            echo 'Registro actualizado';
+try{
+    if ($_POST["operacion"] == "Crear" or $_POST["operacion"] == "Editar") {
+        $stmt = $conexion->prepare("EXECUTE PROC_INS_UPD_HORAEXTRA :id_horEx, :fecha_horaExtra, :tipo, :cantidad, :id_empleado");
+    
+        $resultado = $stmt->execute(
+            array(
+                ':id_horEx'    => $_POST["id_horEx"],
+                ':fecha_horaExtra'    => $_POST["fecha_horaExtra"],
+                ':tipo'    => $_POST["tipo"],
+                ':cantidad'    => $_POST["cantidad"],
+                ':id_empleado'    => $_POST["id_empleado"]
+            )
+        );
+    
+        if (!empty($resultado)) {
+            if ($_POST["operacion"] == "Crear") {
+                echo 'Registro creado';
+            } else {
+                echo 'Registro actualizado';
+            }
         }
+    }
+}catch (Exception $e){
+    if($e -> getMessage() == $k){
+        echo 'El numero de horas extras nocturnas no debe ser mayor a 11 horas';
+    }
+    else{
+        echo 'El numero de horas diurnas no debe ser mayor a 13 horas';
     }
 }
